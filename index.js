9289be383a384765686d2bda24c8e26e756fb8b3
Steven Shaginyan <steven.shaginyan@heroku.com>
2020-05-06 10:29:42 -0700

    Something

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connection: process.env.DATABASE_URL
+});
+
+client.connect();
+
+client.query('SELECT salesforce.transfer()')
+  .then(() => console.log('success'))
+  .catch(error => console.error(error.message))
+  .finaly(() => client.end());
